f206efe6423f774d24a039f3b10737862495fa5e
Wontae Yang <wontaeyang@Monty-Pro.local>
2024-09-28 23:16:35 -0400

    refactor golang module

--- nvim/init.lua
+++ nvim/init.lua
@@ -65,6 +65,8 @@ vim.api.nvim_create_autocmd({ "TermOpen" }, {
   end,
 })
 
+require('golang')
+
 -- setup code completion
 vim.opt.completeopt = 'menu,menuone,noselect'
 local cmp = require('cmp')
@@ -213,49 +215,6 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     map('n', 'gd', vim.lsp.buf.definition, opts)
   end,
 })
-
--- Zig autoformat
-vim.g.zig_fmt_autosave = true
-
--- Golang setup
-function DebugPrintSelected()
-  return function()
-    local selected_text = vim.fn.getreg("")
-    vim.fn.feedkeys('o')
-    vim.fn.feedkeys('fmt.Printf("DEBUG: %v\\n", ')
-    vim.fn.feedkeys(')')
-    vim.api.nvim_input('<Left>')
-  end
-end
-
-function go_test_with_env(test_command)
-  return function()
-    vim.cmd('Dotenv test/test.env')
-    vim.cmd(test_command)
-  end
-end
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "go" },
-  callback = function()
-    vim.opt.list = false
-    vim.opt.expandtab = false
-    map('n', 'gd', ':GoDef<CR>', opts)
-    map('n', 'gt', ':GoDefPop<CR>', opts)
-    map('n', '<leader>t', go_test_with_env("TestNearest"), opts)
-    map('n', '<leader>a', go_test_with_env("TestFile"), opts)
-    map('n', '<leader>l', go_test_with_env("TestLast"), opts)
-    map('n', '<leader>d', DebugPrintSelected(), opts)
-  end,
-})
-
-vim.g.go_def_mapping_enabled = true
-vim.g.go_auto_type_info = true
-vim.g.go_def_mode = 'gopls'
-vim.g.go_info_mode = 'gopls'
-vim.g.go_term_enabled = 'split'
-vim.g.go_doc_keywordprg_enabled = false
-vim.g.go_def_mapping_enabled = false -- remap some vim-go key bindings to prevent colliding with FZF
 
 -- ToggleTerm
 require("toggleterm").setup({
---
+++ nvim/lua/golang.lua
@@ -0,0 +1,38 @@
+function DebugPrintSelected()
+  return function()
+    local selected_text = vim.fn.getreg("")
+    vim.fn.feedkeys('o')
+    vim.fn.feedkeys('fmt.Printf("DEBUG: %v\\n", ')
+    vim.fn.feedkeys(')')
+    vim.api.nvim_input('<Left>')
+  end
+end
+
+function go_test_with_env(test_command)
+  return function()
+    vim.cmd('Dotenv test/test.env')
+    vim.cmd(test_command)
+  end
+end
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "go" },
+  callback = function()
+    vim.opt.list = false
+    vim.opt.expandtab = false
+    map('n', 'gd', ':GoDef<CR>', opts)
+    map('n', 'gt', ':GoDefPop<CR>', opts)
+    map('n', '<leader>t', go_test_with_env("TestNearest"), opts)
+    map('n', '<leader>a', go_test_with_env("TestFile"), opts)
+    map('n', '<leader>l', go_test_with_env("TestLast"), opts)
+    map('n', '<leader>d', DebugPrintSelected(), opts)
+  end,
+})
+
+vim.g.go_def_mapping_enabled = true
+vim.g.go_auto_type_info = true
+vim.g.go_def_mode = 'gopls'
+vim.g.go_info_mode = 'gopls'
+vim.g.go_term_enabled = 'split'
+vim.g.go_doc_keywordprg_enabled = false
+vim.g.go_def_mapping_enabled = false -- remap some vim-go key bindings to prevent colliding with FZF
