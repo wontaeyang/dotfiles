954fd9a832c0d7c1adc52ec875ae955a6ca8161a
Wontae Yang <1811312+wontaeyang@users.noreply.github.com>
2026-03-21 15:40:47 -0400

    add term for claude code

--- nvim/lua/term.lua
+++ nvim/lua/term.lua
@@ -1,9 +1,9 @@
 require("toggleterm").setup({
-  open_mapping = [[<leader>k]],
-  hide_humbers = true,
+  open_mapping = [[<leader>j]],
+  hide_numbers = true,
   start_in_insert = false,
   insert_mappings = false,
-  direction = "float",
+  direction = "tab",
   auto_scroll = true,
   close_on_exit = true,
   float_opts = {
@@ -11,9 +11,22 @@ require("toggleterm").setup({
   },
 })
 
--- Lazygit
 local Terminal = require('toggleterm.terminal').Terminal
-local lazygit  = Terminal:new({
+
+-- Claude Code
+local claude_code = Terminal:new({
+  cmd = "brew upgrade claude-code && claude",
+  direction = "float",
+  close_on_exit = true,
+  hidden = true,
+  float_opts = {
+    border = "curved",
+  },
+})
+map("n", "<leader>k", function() claude_code:toggle() end, opts)
+
+-- Lazygit
+local lazygit = Terminal:new({
   cmd = "lazygit",
   dir = "git_dir",
   direction = "float",
@@ -23,11 +36,4 @@ local lazygit  = Terminal:new({
     border = "curved",
   },
 })
-
-local function lazygit_toggle()
-  return function()
-    lazygit:toggle()
-  end
-end
-
-map("n", "<leader>l", lazygit_toggle(), opts)
+map("n", "<leader>l", function() lazygit:toggle() end, opts)
